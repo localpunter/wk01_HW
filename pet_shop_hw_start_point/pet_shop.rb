8f6c9453dd425b3188bfd3c12a32625e956992b3
Alan Hunter <localpunter@sky.com>
2018-08-06 17:05:23 +0100

    weekend homework re-added

--- pet_shop_hw_start_point/pet_shop.rb
+++ pet_shop_hw_start_point/pet_shop.rb
@@ -44,10 +44,20 @@ return pets_by_breed
 
 end
 
+# # def find_pet_by_name(pet_shop, name)
+# #     pet_by_name = [] #array not required as only 1 item is looked for.
+# #     for pet in pet_shop[:pets]
+# #       pet_by_name << pet if pet[:name]==name
+# #     end
+# # return pet_by_name
+#
+# end
+
 def find_pet_by_name(pet_shop, name)
-    pet_by_name = []
+    pet_by_name = nil #this set the bucket as empty until the arguement
+    #is returned as true.
     for pet in pet_shop[:pets]
-      pet_by_name << pet if pet[:name]==name
+      pet_by_name = pet if pet[:name]==name
     end
 return pet_by_name
 
@@ -60,14 +70,24 @@ end
 #   pet [:pets][:name].length
 # end
 
-def find_pet_by_name(pet_shop, name)
-  return name[:pet_shop][:pets]
+# def find_pet_by_name(pet_shop, name)
+#   return name[:pet_shop][:pets]
+#
+# end
 
+
+def remove_pet_by_name(pet_shop, pet_name)
+  pet_to_delete = find_pet_by_name(pet_shop, pet_name)
+  pet_shop[:pets].delete(pet_to_delete)
+end
+
+def add_pet_to_stock(pet_shop, pet)
+  pet_shop[:pets].push(pet)
 end
 
 def customer_cash(customers)
 return customers[:cash]
-#Not sure why this returns 1000 and not 1050 in the spec tab?
+
 end
 
 def remove_customer_cash(customers, cash)
@@ -98,4 +118,19 @@ def add_pet_to_customer(customers, new_pet)
   customers[:pets]<< new_pet
   customers[:pets].length
 
+end
+
+def customer_can_afford_pet(customer, pet)
+  return customer[:cash] >= pet[:price]
+end
+
+def sell_pet_to_customer(pet_shop, pet, customer)
+  return if (pet == nil)
+  return if !(customer_can_afford_pet(customer, pet))
+
+  add_pet_to_customer(customer,pet)
+  add_or_remove_cash(pet_shop, pet[:price])
+  remove_customer_cash(pet_shop, pet[:price])
+  remove_pet_by_name(pet_shop, pet[:name])
+  increase_pets_sold(pet_shop, 1)
 end
